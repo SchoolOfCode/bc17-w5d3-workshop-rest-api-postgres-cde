ed45305fd33c6a6e98825ee9edca1c914b52b282
dom-m17 <dominicmaynard00@gmail.com>
2024-07-31 16:51:40 +0100

    creating post function

--- books.js
+++ books.js
@@ -32,6 +32,11 @@ export async function getBookById(id) {
 
 export async function createBook(book) {
   // Query the database to create a book and return the newly created book
+  const queryText = "INSERT INTO books (title, published_date, author_id) VALUES ($1,$2,$3)"
+
+  const result = await pool.query(queryText, [book.title, book.published_date, book.author_id]);
+
+  return book
 }
 
 export async function updateBookById(id, updates) {
